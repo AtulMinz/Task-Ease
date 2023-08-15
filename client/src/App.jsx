74458d22ab7738089cb398c97c4ff418ec0dab61
AtulMinz <atulminz.jsr@gmail.com>
2023-08-15 17:48:17 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <button className='rounded-md bg-green-300 px-8 py-6'>
+        Dark Mode
+      </button>
+    </div>
+  )
+}
+
+export default App
